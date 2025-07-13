e538d2f81c3e7797bf115eabbada34a77f202896
FadilAlfaizii <alfaizifadil100@gmail.com>
2025-07-13 15:28:34 +0700

    aaa another one

--- src/app/page-signup/page-author/page.js
+++ src/app/page-signup/page-author/page.js
@@ -2,7 +2,7 @@ import Link from "next/link"
 import Image from "next/image"
 import Layout from "@/components/layout/Layout"
 import Pagination from "@/components/elements/Pagination"
-import Hero6 from "@/components/sections/Hero6"
+import Hero6 from "@/components/sections/Hero3"
 
 import data from "@/utils/blogData"
 
